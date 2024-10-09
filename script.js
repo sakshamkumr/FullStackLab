d0a893bfb8f445143c31a72e89bc6b6c29113c90
sakshamkumr <122541184+sakshamkumr@users.noreply.github.com>
2024-10-09 15:04:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var studentId=3465;
+let studentName="Sachin";
+const studentFee=400000;
+
+console.log(studentId);
+console.log(studentName);
+
+function sum() {
+    let sum=55;
+    if(sum==55){
+        var fullname="abcde";
+    }
+    console.log(fullname);
+    
+}
+sum();
+console.log(fullname);
+
+// const salary=45678;   // cannot re-declare const type variable
+// let salary=47856;
+
